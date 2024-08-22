19a06103721d93b7a50d869332214ce6f7c97b75
SamaraPalladino <luizamaradelg@gmail.com>
2024-08-22 12:59:26 -0300

    primeiros passos pipeline

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-function soma(num1, num2) {
-    return num1 + num2;
+
+document.getElementById('aplicarDesconto10').addEventListener('click', function() {
+  var valorCompra = parseFloat(document.getElementById('valorCompra').value)
+  if (isNaN(valorCompra) || valorCompra <= 0) {
+    document.getElementById('resultado').innerText = 'Por favor, insira um valor válido para a compra.';
+    return;
   }
-  module.exports = soma;
+var desconto = valorCompra * 0.10;
+var valorComDesconto = valorCompra - desconto;
+})
+
+document.getElementById('aplicarDesconto5').addEventListener('click', function() {
+  var valorCompra = parseFloat(document.getElementById('valorCompra').value)
+  var desconto = valorCompra * 0.5;
+var valorComDesconto = valorCompra - desconto;
+})
+
+ 
